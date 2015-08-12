c2b0d64f1a73e48257f38ced902baece0960e5ff
J. Francisco Raposerias <rapofran@gmail.com>
2015-08-12 09:49:51 -0300

    More changes, first version of scrapping title

--- plugins/title.js
+++ plugins/title.js
@@ -2,26 +2,41 @@
 
 'use strict';
 
+var _ = require('underscore');
 var og = require('open-graph');
+
+// hack
+String.prototype.scan = function(regex) {
+    var result = []
+
+    this.replace(regex, function() {
+        result.push(Array.prototype.slice.call(arguments, 1, -2));
+    });
+
+    return result;
+}
+
+function getTitle(link) {
+    var title = '';
+
+    og(link, function(err, meta){
+        title = meta.title;
+    })
+
+    // does't have open-graph
+    if (title === '') {
+        //srap manually
+    } else {
+        bot.say(title);
+    }
+}
 
 function scrap(nick, text, message) {
     var bot = this;
 
-    if(/^(http|https):\/\//.test(text)) {
-        var title = '';
-
-        og(url, function(err, meta){
-            title = meta.title;
-        })
-
-        // does't have open-graph
-        if (title === '') {
-            //srap manually
-        }
-
-        bot.say(title);
-    }
+    _.each(text.scan(/^(http|https):\/\//g), getTitle);
 }
+
 
 function setupPlugin(bot) {
     bot.on(bot.events.MESSAGE, scrap);
